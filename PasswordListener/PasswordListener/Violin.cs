ff659e46d2f32ceaf922245b14186a038448dfca
lior matsliah <liormatsliah1985@gmail.com>
2018-02-03 20:16:00 +0200

    flow

---
+++ PasswordListener/PasswordListener/Violin.cs
@@ -0,0 +1,56 @@
+﻿using Fiddler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Fiddler.RequiredVersion("2.3.5.0")]
+
+namespace PasswordListener
+{
+    public class Violin : IAutoTamper    
+    {
+        string sUserAgent = "";
+
+        public Violin()
+        {
+            sUserAgent = "Violin";
+        }
+
+        public void OnLoad()
+        {
+            /* Load your UI here */
+        }
+
+        public void OnBeforeUnload()
+        {
+
+        }
+
+        public void AutoTamperRequestBefore(Session oSession)
+        {
+            oSession.oRequest["User-Agent"] = sUserAgent;
+        }
+
+        public void AutoTamperRequestAfter(Session oSession)
+        {
+
+        }
+
+        public void AutoTamperResponseBefore(Session oSession)
+        {
+
+        }
+
+        public void AutoTamperResponseAfter(Session oSession)
+        {
+
+        }
+
+        public void OnBeforeReturningError(Session oSession)
+        {
+
+        }
+    }
+}
